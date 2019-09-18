c0ac4618fec46e9035690a31078200948f41db97
Bharat Kumar <bharat@atglobalinc.com>
2019-09-19 02:27:40 +0530

    Implemented QrScanner code

--- app/src/main/java/com/example/qrscanner/QRScannerFragment.kt
+++ app/src/main/java/com/example/qrscanner/QRScannerFragment.kt
@@ -7,14 +7,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-//import com.androchef.qrcodescanner.ui.dialogs.QrCodeResultDialog
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+//import com.example.qrcodescanner.ui.dialogs.QrCodeResultDialog
 import kotlinx.android.synthetic.main.fragment_qrscanner.view.*
-//import me.dm7.barcodescanner.zbar.Result
-//import me.dm7.barcodescanner.zbar.ZBarScannerView
+import me.dm7.barcodescanner.zbar.Result
+import me.dm7.barcodescanner.zbar.ZBarScannerView
 
-//class QRScannerFragment : Fragment(), ZBarScannerView.ResultHandler {
+class QRScannerFragment : Fragment(), ZBarScannerView.ResultHandler {
 
-    class QRScannerFragment : Fragment() {
+  //  class QRScannerFragment : Fragment() {
 
     companion object {
 
@@ -23,41 +25,40 @@ import kotlinx.android.synthetic.main.fragment_qrscanner.view.*
         }
     }
 
-//    private lateinit var mView: View
-//
-//    lateinit var scannerView: ZBarScannerView
-//
-//    lateinit var resultDialog: QrCodeResultDialog
-//
-//    private lateinit var dbHelperI: DbHelperI
+    private lateinit var mView: View
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_qrscanner, container, false)
+    lateinit var scannerView: ZBarScannerView
 
-    }
+
+override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    mView = inflater.inflate(R.layout.fragment_qrscanner, container, false)
+    initializeQRCamera()
+    onClicks()
+    return mView.rootView
+}
 //
 //    private fun init() {
 //        dbHelperI = DbHelper(QrResultDataBase.getAppDatabase(context!!)!!)
 //    }
-//
+
 //    private fun initViews() {
 //        initializeQRCamera()
 //        setResultDialog()
 //    }
-//
-//    private fun initializeQRCamera() {
-//        scannerView = ZBarScannerView(context)
-//        scannerView.setResultHandler(this)
-//        scannerView.setBackgroundColor(ContextCompat.getColor(context!!, R.color.colorTranslucent))
-//        scannerView.setBorderColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
-//        scannerView.setLaserColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
-//        scannerView.setBorderStrokeWidth(10)
-//        scannerView.setSquareViewFinder(true)
-//        scannerView.setupScanner()
-//        scannerView.setAutoFocus(true)
-//        startQRCamera()
-//        mView.containerScanner.addView(scannerView)
-//    }
+
+    private fun initializeQRCamera() {
+        scannerView = ZBarScannerView(context)
+        scannerView.setResultHandler(this)
+        scannerView.setBackgroundColor(ContextCompat.getColor(context!!, R.color.colorTranslucent))
+        scannerView.setBorderColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
+        scannerView.setLaserColor(ContextCompat.getColor(context!!, R.color.colorPrimaryDark))
+        scannerView.setBorderStrokeWidth(10)
+        scannerView.setSquareViewFinder(true)
+        scannerView.setupScanner()
+        scannerView.setAutoFocus(true)
+        startQRCamera()
+        mView.containerScanner.addView(scannerView)
+    }
 //
 //    private fun setResultDialog() {
 //        resultDialog = QrCodeResultDialog(context!!)
@@ -67,11 +68,8 @@ import kotlinx.android.synthetic.main.fragment_qrscanner.view.*
 //            }
 //        })
 //    }
-//
-//
-//    override fun handleResult(rawResult: Result?) {
-//        onQrResult(rawResult?.contents)
-//    }
+
+
 //
 //    private fun onQrResult(contents: String?) {
 //        if (contents.isNullOrEmpty())
@@ -79,7 +77,7 @@ import kotlinx.android.synthetic.main.fragment_qrscanner.view.*
 //        else
 //            saveToDataBase(contents)
 //    }
-//
+
 //    private fun showToast(message: String) {
 //        Toast.makeText(context!!, message, Toast.LENGTH_SHORT).show()
 //    }
@@ -89,52 +87,61 @@ import kotlinx.android.synthetic.main.fragment_qrscanner.view.*
 //        val qrResult = dbHelperI.getQRResult(insertedResultId)
 //        resultDialog.show(qrResult)
 //    }
-//
-//    private fun startQRCamera() {
-//        scannerView.startCamera()
-//    }
-//
+
+    private fun startQRCamera() {
+        scannerView.startCamera()
+    }
+
 //    private fun resetPreview() {
 //        scannerView.stopCamera()
 //        scannerView.startCamera()
 //        scannerView.stopCameraPreview()
 //        scannerView.resumeCameraPreview(this)
 //    }
-//
-//    private fun onClicks() {
-//        mView.flashToggle.setOnClickListener {
-//            if (mView.flashToggle.isSelected) {
-//                offFlashLight()
-//            } else {
-//                onFlashLight()
-//            }
-//        }
-//    }
-//
-//    private fun onFlashLight() {
-//        mView.flashToggle.isSelected = true
-//        scannerView.flash = true
-//    }
-//
-//    private fun offFlashLight() {
-//        mView.flashToggle.isSelected = false
-//        scannerView.flash = false
-//    }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        scannerView.setResultHandler(this)
-//        scannerView.startCamera()
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        scannerView.stopCamera()
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        scannerView.stopCamera()
-//    }
+
+    private fun onClicks() {
+        mView.flashToggle.setOnClickListener {
+            if (mView.flashToggle.isSelected) {
+                offFlashLight()
+            } else {
+                onFlashLight()
+            }
+        }
+    }
+
+    private fun onFlashLight() {
+        mView.flashToggle.isSelected = true
+        scannerView.flash = true
+    }
+
+    private fun offFlashLight() {
+        mView.flashToggle.isSelected = false
+        scannerView.flash = false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        scannerView.setResultHandler(this)
+        scannerView.startCamera()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        scannerView.stopCamera()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scannerView.stopCamera()
+    }
+
+override fun handleResult(rawResult: Result?)
+{
+
+    Toast.makeText(context!!,rawResult?.contents,Toast.LENGTH_LONG).show()
+    scannerView.resumeCameraPreview(this)
+   // Toast.makeText(context!!,rawResult,Toast.LENGTH_LONG).show()
+}
+
 
 }
